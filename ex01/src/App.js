af5e5842cd649173b32d4248cfccee1c24368df4
jadilovic <adilovic79@yahoo.com>
2021-03-30 10:07:58 +0200

    adding exercises 1 through 3

---
+++ ex01/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Plant from './Plant.js'
+
+function App() {
+  return (
+    <div className="container">
+      <Plant />
+    </div>
+  );
+}
+
+export default App;
